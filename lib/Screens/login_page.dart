c4c0c5a14f64b0b069bae1f20101af2f6383d13a
pravi <pravi@DESKTOP-JU2ECM4>
2025-05-08 13:30:53 +0530

    up

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:voicefirstuser/Screens/UserHomeScreen.dart';
-import 'package:voicefirstuser/Screens/home_screen.dart'; // Update this with actual screen paths
+import 'package:voicefirstuser/User/Dashboard/UserHomeScreen.dart';
+import 'package:voicefirstuser/Admin/Dashboard/home_screen.dart'; // Update this with actual screen paths
 
 void main() {
   runApp(MyApp());
@@ -53,7 +53,7 @@ class _LoginScreenState extends State<LoginScreen> {
     } else if (username == userUsername && password == userPassword) {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const Userhomescreen()),
+        MaterialPageRoute(builder: (context) => Userhomescreen()),
       );
     } else {
       setState(() {
@@ -65,8 +65,13 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   @override
+  @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -79,169 +84,167 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
         ),
         child: SafeArea(
-          child: Column(
-            children: [
-              SizedBox(height: 60),
-              Center(
-                child: Column(
-                  children: [
-                    Text(
-                      'Hello',
-                      style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    Text(
-                      'Welcome Back!',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.black87,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 40),
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 30),
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                  ),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    elevation: 8,
-                    child: Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Login Account',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            'Enter your credentials below.',
-                            style: TextStyle(color: Colors.grey[600]),
-                          ),
-                          SizedBox(height: 20),
-                          TextField(
-                            controller: _emailController,
-                            decoration: InputDecoration(
-                              labelText: 'Username',
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.person),
-                            ),
-                          ),
-                          SizedBox(height: 15),
-                          TextField(
-                            controller: _passwordController,
-                            obscureText: !_isPasswordVisible,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.lock),
-                              suffixIcon: IconButton(
-                                icon: Icon(
-                                  _isPasswordVisible
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    _isPasswordVisible = !_isPasswordVisible;
-                                  });
-                                },
-                              ),
-                            ),
-                          ),
-                          if (_errorMessage.isNotEmpty) ...[
-                            SizedBox(height: 10),
-                            Center(
-                              child: Text(
-                                _errorMessage,
-                                style: TextStyle(color: Colors.red),
-                              ),
-                            ),
-                          ],
-                          SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: IntrinsicHeight(
+                    child: Column(
+                      children: [
+                        SizedBox(height: screenHeight * 0.08),
+                        Center(
+                          child: Column(
                             children: [
-                              Row(
-                                children: [
-                                  Icon(Icons.check_circle,
-                                      color: Colors.green, size: 20),
-                                  SizedBox(width: 5),
-                                  Text('Save Password'),
-                                ],
+                              Text(
+                                'Hello',
+                                style: TextStyle(
+                                  fontSize: screenWidth * 0.12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
                               ),
                               Text(
-                                'Forgot Password?',
+                                'Welcome Back!',
                                 style: TextStyle(
-                                  color: Colors.blue,
-                                  fontWeight: FontWeight.w500,
+                                  fontSize: screenWidth * 0.05,
+                                  color: Colors.black87,
                                 ),
                               ),
                             ],
                           ),
-                          SizedBox(height: 30),
-                          SizedBox(
-                            width: double.infinity,
-                            child: InkWell(
-                              onTap: _isLoading ? null : _login,
-                              borderRadius: BorderRadius.circular(10),
-                              child: Container(
-                                padding: EdgeInsets.symmetric(vertical: 15),
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    colors: [
-                                      Color.fromARGB(255, 17, 17, 17),
-                                      Color.fromARGB(255, 56, 56, 55)
-                                    ],
-                                    begin: Alignment.topLeft,
-                                    end: Alignment.bottomRight,
+                        ),
+                        SizedBox(height: screenHeight * 0.05),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 25, vertical: 10),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            elevation: 8,
+                            child: Padding(
+                              padding: EdgeInsets.all(20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Login Account',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                alignment: Alignment.center,
-                                child: _isLoading
-                                    ? CircularProgressIndicator(
-                                        color: Colors.white,
-                                      )
-                                    : Text(
-                                        'Login Account',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
+                                  SizedBox(height: 5),
+                                  Text(
+                                    'Enter your credentials below.',
+                                    style: TextStyle(color: Colors.grey[600]),
+                                  ),
+                                  SizedBox(height: 20),
+                                  TextField(
+                                    controller: _emailController,
+                                    decoration: InputDecoration(
+                                      labelText: 'Username',
+                                      border: OutlineInputBorder(),
+                                      prefixIcon: Icon(Icons.person),
+                                    ),
+                                  ),
+                                  SizedBox(height: 15),
+                                  TextField(
+                                    controller: _passwordController,
+                                    obscureText: !_isPasswordVisible,
+                                    decoration: InputDecoration(
+                                      labelText: 'Password',
+                                      border: OutlineInputBorder(),
+                                      prefixIcon: Icon(Icons.lock),
+                                      suffixIcon: IconButton(
+                                        icon: Icon(
+                                          _isPasswordVisible
+                                              ? Icons.visibility
+                                              : Icons.visibility_off,
                                         ),
+                                        onPressed: () {
+                                          setState(() {
+                                            _isPasswordVisible =
+                                                !_isPasswordVisible;
+                                          });
+                                        },
                                       ),
+                                    ),
+                                  ),
+                                  if (_errorMessage.isNotEmpty) ...[
+                                    SizedBox(height: 10),
+                                    Center(
+                                      child: Text(
+                                        _errorMessage,
+                                        style: TextStyle(color: Colors.red),
+                                      ),
+                                    ),
+                                  ],
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Icon(Icons.check_circle,
+                                              color: Colors.green, size: 20),
+                                          SizedBox(width: 5),
+                                          Text('Save Password'),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 30),
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: InkWell(
+                                      onTap: _isLoading ? null : _login,
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Container(
+                                        padding:
+                                            EdgeInsets.symmetric(vertical: 15),
+                                        decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                            colors: [
+                                              Color.fromARGB(255, 17, 17, 17),
+                                              Color.fromARGB(255, 56, 56, 55)
+                                            ],
+                                            begin: Alignment.topLeft,
+                                            end: Alignment.bottomRight,
+                                          ),
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        alignment: Alignment.center,
+                                        child: _isLoading
+                                            ? CircularProgressIndicator(
+                                                color: Colors.white,
+                                              )
+                                            : Text(
+                                                'Login Account',
+                                                style: TextStyle(
+                                                  fontSize: 16,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
-                          SizedBox(height: 20),
-                          Center(
-                            child: Text(
-                              'Create New Account',
-                              style: TextStyle(color: Colors.black87),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                        Spacer(),
+                      ],
                     ),
                   ),
                 ),
-              ),
-            ],
+              );
+            },
           ),
         ),
       ),
---
+++ lib/User/Dashboard/BottomNavBar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:voicefirstuser/User/Dashboard/Profile.dart';
+import 'package:voicefirstuser/User/Dashboard/QrScanner.dart';
+
+import 'package:voicefirstuser/User/Dashboard/UserHomeScreen.dart';
+
+// import other screens like SavedScreen, ScanScreen if you have them
+class Bottomnavbar extends StatefulWidget {
+  @override
+  _BottomnavbarState createState() => _BottomnavbarState();
+}
+
+class _BottomnavbarState extends State<Bottomnavbar> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    Userhomescreen(), // This might be missing or commented
+    Center(child: Text('Saved Page', style: TextStyle(color: Colors.white))),
+    QrScanScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        selectedItemColor: Color(0xFFFCC737),
+        unselectedItemColor: Colors.white,
+        selectedLabelStyle: TextStyle(color: Color(0xFFFCC737)),
+        unselectedLabelStyle: TextStyle(color: Colors.white),
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() => _currentIndex = index);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: 'Nearby',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: 'Saved'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner),
+            label: 'Scan',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
